e4ce65578fa513efda9acfa3a98e0392395a0ea7
David Marshall <david.marshall@wwt.com>
2022-02-02 11:37:29 -0500

    attempt to get grid to be correct width on iphone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from './services/config.service';
 import { globalConfig, updateConfig } from '../app/config'
+import { Config } from './models/config';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,14 @@ export class AppComponent implements OnInit {
 
   artist;
   imageRootURI;
+  bgColor;
 
   constructor (private configSvc: ConfigService) {}
 
   ngOnInit(): void {
-    this.configSvc.getConfig().subscribe(c => {
+    this.configSvc.getConfig().subscribe((c:Config) => {
       this.imageRootURI = c.imageRootURI;
+      this.bgColor = c.backgroundColor;
       this.artist = c.artist;
       this.configSvc.addConfig(c); // other components need this config so add it back to the service.
       updateConfig(c);
